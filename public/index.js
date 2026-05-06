1daa2b5743d9f921791b1cce8c2af884f786b2be
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2026-05-06 15:28:13 +0200

    Remove unnessecary parts of project

--- public/index.js
+++ public/index.js
@@ -1,36 +1,20 @@
-import Dialog from './Dialog.js';
-import StartButton from './StartButton.js';
 import { setup, draw, keyPressed } from './sketch.js';
 
 console.clear();
-const optionsDialog = new Dialog('#options', '#optionsBtn');
-const startButton = new StartButton('#startBtn', () => {
-    optionsDialog.close();
-    startButton.disable();
-    document.querySelector('#game').style.display = 'block';
-    document.querySelector('#landing-page').style.display = 'none';
-    document.querySelector('footer').style.display = 'none';
-    document.body.style.overflow = 'hidden';
 
-    const p5 = window.p5;
-    if (!p5) {
-        throw new Error(
-            'p5.js not found. Make sure to include it in your HTML file.',
-        );
-    }
-
-    let p;
-    const appElement = document.querySelector('#game');
-    new p5((p5) => {
-        p = p5;
-
-        p.setup = () => setup(p);
-        p.draw = () => draw(p);
-
-        p.keyPressed = () => keyPressed(p);
-    }, appElement);
-});
-
-if (localStorage.getItem('debug') != null) {
-    startButton.button.dispatchEvent(new Event('click'));
+const p5 = window.p5;
+if (!p5) {
+    throw new Error(
+        'p5.js not found. Make sure to include it in your HTML file.',
+    );
 }
+
+let p;
+new p5((p5) => {
+    p = p5;
+
+    p.setup = () => setup(p);
+    p.draw = () => draw(p);
+
+    p.keyPressed = () => keyPressed(p);
+});
