73c966be6048d08140b2034760673ab11f30c106
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2026-05-13 16:27:27 +0200

    Add more levels

---
+++ documentation/Sprint-04.md
@@ -0,0 +1,15 @@
+# Sommerprojekt 2026 - Sprint 04
+
+Benjamin, 2AHITM
+
+## Fortschritt
+
+- Level Design Levels: 2, 3
+- [ ] Gegner
+- [ ] Item System
+- [ ] Save/Load System
+- [ ] Store
+
+## GitHub Insights
+
+## Ziele für nächsten Sprint
--- project/levels.js
+++ project/levels.js
@@ -29,5 +29,100 @@ export default [
             Gren/Flr4 Snow/None Snow/None Brdg/None SnPa/None
             Gren/TreS SnQu/None SnRo/None Empt/None SnSu/None
         `,
+        transfers: [
+            {
+                from: {
+                    x: 0,
+                    y: 0,
+                },
+                to: {
+                    level: 'home',
+                    x: 2,
+                    y: 1,
+                },
+            },
+            {
+                from: {
+                    x: 4,
+                    y: 4,
+                },
+                to: {
+                    level: 'level2',
+                    x: 0,
+                    y: 3,
+                },
+            },
+        ],
+    },
+    {
+        name: 'level2',
+        string: `
+            Empt/None Empt/None Empt/None Snow/None Snow/None Snow/None Empt/None Empt/None
+            Empt/None Empt/None Snow/None Snow/SgnR Snow/None Snow/None Snow/None Empt/None
+            Empt/None Snow/None SnRo/None SnSu/None Snow/None SnRo/None Snow/None Wood/None
+            Brdg/Plyr SnSu/None Snow/None Snow/None Snow/Path SnSu/None Snow/None Snow/None
+            Empt/None Snow/None Snow/None Snow/Spar Snow/None Snow/Path SnRo/None Snow/None
+            Empt/None Snow/None Snow/Path Snow/None SnPa/Path Snow/None SnSu/None Snow/None
+            Empt/None Empt/None Snow/None SnQu/None SnSu/None Snow/None Snow/None Empt/None
+            Empt/None Empt/None Empt/None Snow/None Snow/None Snow/None Empt/None Empt/None
+        `,
+        transfers: [
+            {
+                from: {
+                    x: 0,
+                    y: 3,
+                },
+                to: {
+                    level: 'level1',
+                    x: 4,
+                    y: 4,
+                },
+            },
+            {
+                from: {
+                    x: 7,
+                    y: 2,
+                },
+                to: {
+                    level: 'level3',
+                    x: 0,
+                    y: 1,
+                },
+            },
+        ],
+    },
+    {
+        name: 'level3',
+        string: `
+            Empt/None Gren/Path Gren/None Gren/None Snow/None Snow/None Empt/None
+            Wood/Plyr Gren/Path Gren/None Gren/None Gren/None Snow/None Snow/None
+            Empt/None Gren/Flr4 Gren/None Gren/None Snow/TreW Snow/None SnPa/None
+            Empt/None Gren/Flr2 Gren/Flr4 Snow/None Snow/None SnQu/None SnPa/None
+            Empt/None Empt/None Empt/None Empt/None SnPa/None Empt/None Empt/None
+        `,
+        transfers: [
+            {
+                from: {
+                    x: 0,
+                    y: 1,
+                },
+                to: {
+                    level: 'level2',
+                    x: 7,
+                    y: 2,
+                },
+            },
+            {
+                from: {
+                    x: 4,
+                    y: 4,
+                },
+                to: {
+                    level: 'home',
+                    x: 1,
+                    y: 1,
+                },
+            },
+        ],
     },
 ];
